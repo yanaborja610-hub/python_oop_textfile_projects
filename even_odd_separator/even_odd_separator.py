342c556f2e3f63aa09efb9dd5fbfef9deef91b08
yanab <yanaborja610@gmail.com>
2026-04-22 23:00:58 +0800

    Added sort function so that the numbers is sorted from lowest to highest

--- even_odd_separator/even_odd_separator.py
+++ even_odd_separator/even_odd_separator.py
@@ -14,11 +14,13 @@ class EvenOddSeparator:
                     self.odd_numbers.append(number)
 
     def write_even_file(self):
+        self.even_numbers.sort()
         with open('even.txt', 'w') as even_file:
             for numbers in self.even_numbers:
                 even_file.write(str(numbers) + '\n')
 
     def write_odd_file(self):
+        self.odd_numbers.sort()
         with open('odd.txt', 'w') as odd_file:
             for numbers in self.odd_numbers:
                 odd_file.write(str(numbers) + '\n')
