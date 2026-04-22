7cca9c56e04d7a7e9e4a9832f56940ce6a6d8387
yanab <yanaborja610@gmail.com>
2026-04-23 00:01:56 +0800

    Added the methods for finding the highest gwa

--- highest_gwa_finder/highest_gwa_finder_class.py
+++ highest_gwa_finder/highest_gwa_finder_class.py
@@ -1,6 +1,25 @@
 class HighestGwaFinder:
-    def __init__(self, student_name, student_gwa):
-        self.student_name = student_name
+    def __init__(self, student_gwa):
         self.student_gwa = student_gwa
         self.gwa_finder = []
-        
+
+    def finding_highest_gwa(self):
+        try:
+            with open(self.student_gwa, 'r') as file:
+                highest_name = " "
+                highest_gwa = float("inf")
+
+                for name_gwa in file:
+                    name_gwa = name_gwa.strip()
+                    name, gwa = name_gwa.split(",")
+                    gwa = float(gwa.strip())
+
+                    if gwa < highest_gwa:
+                        highest_gwa = gwa
+                        highest_name = name.strip()
+
+        except FileNotFoundError:
+            print("File not found.")
+            return None, None
+
+        return highest_name, highest_gwa
