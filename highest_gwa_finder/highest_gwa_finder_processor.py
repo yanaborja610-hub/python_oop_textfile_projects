81d32b4314f377f0c48d8fd9fe44a52932f73fe2
yanab <yanaborja610@gmail.com>
2026-04-23 01:02:17 +0800

    Added the menu options and the input function under a while loop

--- highest_gwa_finder/highest_gwa_finder_processor.py
+++ highest_gwa_finder/highest_gwa_finder_processor.py
@@ -2,3 +2,12 @@ from highest_gwa_finder_class import HighestGwaFinder
 
 finder = HighestGwaFinder("students_with_their_gwa.txt")
 finder.load_students()
+
+while True:
+    print("\n===== MENU =====")
+    print("1. Show Top Student")
+    print("2. Show Top 3 Students")
+    print("3. Show All Students")
+    print("4. Exit")
+
+    choice = input("Enter choice: ")
