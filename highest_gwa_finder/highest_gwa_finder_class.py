c33ee6014b7c953dd9e4f2c6ac54ce90b49ae23d
yanab <yanaborja610@gmail.com>
2026-04-22 23:42:31 +0800

    Added file for class, defined and initialized class

---
+++ highest_gwa_finder/highest_gwa_finder_class.py
@@ -0,0 +1,6 @@
+class HighestGwaFinder:
+    def __init__(self, student_name, student_gwa):
+        self.student_name = student_name
+        self.student_gwa = student_gwa
+        self.gwa_finder = []
+        
