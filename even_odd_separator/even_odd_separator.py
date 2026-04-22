bec87e61e3e04b27999735307cb73d1c9e17b75b
yanab <yanaborja610@gmail.com>
2026-04-22 22:40:52 +0800

    For reading numbers and implementing their logic

--- even_odd_separator/even_odd_separator.py
+++ even_odd_separator/even_odd_separator.py
@@ -3,3 +3,12 @@ class EvenOddSeparator:
         self.numbers = numbers
         self.odd_numbers = []
         self.even_numbers = []
+
+    def process_numbers(self):
+        with(open(self.numbers, 'r')) as numbers_file:
+            for numbers in numbers_file:
+                numbers = numbers.strip()
+                if number % 2 == 0:
+                    self.even_numbers.append(numbers)
+                else:
+                    self.odd_numbers.append(numbers)
