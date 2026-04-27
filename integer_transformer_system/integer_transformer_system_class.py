1497a8eaa666609bcb195f65270ceff3f839060d
yanab <yanaborja610@gmail.com>
2026-04-27 23:23:52 +0800

    Added the user input function so that we could get user interaction

--- integer_transformer_system/integer_transformer_system_class.py
+++ integer_transformer_system/integer_transformer_system_class.py
@@ -4,3 +4,16 @@ class IntegerTransformerSystem:
         self.numbers = []
         self.even_squared = []
         self.odd_cubed = []
+
+    def get_user_input(self):
+        print("Enter 20 Integers:\n ")
+
+        for i in range(1, 21):
+            while True:
+                try:
+                    numbers = (int(input(f"Enter Number {i}: ")))
+                    self.numbers.append(numbers)
+                    break
+
+                except ValueError:
+                    print("Invalid Input. Please enter an integer")
