4ab3d4137e59f0d6aaeb05dc892d54996706020c
yanab <yanaborja610@gmail.com>
2026-04-27 23:51:49 +0800

    Called the class and some functions to actually run the program

---
+++ integer_transformer_system/even_squared_and_odd_cubed_main_program.py
@@ -0,0 +1,8 @@
+from integer_transformer_system_class import IntegerTransformerSystem
+
+def main():
+    system = IntegerTransformerSystem("integers.txt")
+    system.run()
+
+if __name__ == "__main__":
+    main()
